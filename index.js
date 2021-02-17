2cf1eace024af91f1ce12103828dee6a6d9f6173
Aayush KUmar Gupta <ayush.gupta@jellyfishtechnologies.com>
2021-02-17 16:15:05 +0530

    Commit

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ fetch(url)
             var td4 = document.createElement("TD");
             var td5 = document.createElement("TD");
             var td6 = document.createElement("TD");
+            var td7 = document.createElement("TD");
+            var td8 = document.createElement("TD");
 
             var id = document.createTextNode(user.id);
             var name = document.createTextNode(user.name);
@@ -19,6 +21,8 @@ fetch(url)
             var email = document.createTextNode(user.email);
             var phone = document.createTextNode(user.phone);
             var website = document.createTextNode(user.website);
+            var company = document.createTextNode(user.company.name);
+            var city = document.createTextNode(user.address.city);
 
             td1.appendChild(id);
             td2.appendChild(name);
@@ -26,6 +30,8 @@ fetch(url)
             td4.appendChild(email);
             td5.appendChild(phone);
             td6.appendChild(website);
+            td7.appendChild(company);
+            td8.appendChild(city);
 
             tr.appendChild(td1);
             tr.appendChild(td2);
@@ -33,6 +39,8 @@ fetch(url)
             tr.appendChild(td4);
             tr.appendChild(td5);
             tr.appendChild(td6);
+            tr.appendChild(td7);
+            tr.appendChild(td8);
             
             document.getElementById("userData").appendChild(tr);
         })
