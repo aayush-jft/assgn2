70e762ebe44c0b3a6c5b4ecf099959ca5c9d5d36
Aayush KUmar Gupta <ayush.gupta@jellyfishtechnologies.com>
2021-02-17 16:06:01 +0530

    Third Commit

--- index.js
+++ index.js
@@ -6,34 +6,33 @@ fetch(url)
         data.forEach(function(user) {
             var tr = document.createElement("TR");
 
-                var td1 = document.createElement("TD");
-                var td2 = document.createElement("TD");
-                var td3 = document.createElement("TD");
-                var td4 = document.createElement("TD");
-                var td5 = document.createElement("TD");
-                var td6 = document.createElement("TD");
+            var td1 = document.createElement("TD");
+            var td2 = document.createElement("TD");
+            var td3 = document.createElement("TD");
+            var td4 = document.createElement("TD");
+            var td5 = document.createElement("TD");
+            var td6 = document.createElement("TD");
 
-                var id = document.createTextNode(user.id);
-                var name = document.createTextNode(user.name);
-                var username = document.createTextNode(user.username);
-                var email = document.createTextNode(user.email);
-                var phone = document.createTextNode(user.phone);
-                var website = document.createTextNode(user.wesite);
+            var id = document.createTextNode(user.id);
+            var name = document.createTextNode(user.name);
+            var username = document.createTextNode(user.username);
+            var email = document.createTextNode(user.email);
+            var phone = document.createTextNode(user.phone);
+            var website = document.createTextNode(user.website);
 
-                td1.appendChild(id);
-                td2.appendChild(name);
-                td3.appendChild(username);
-                td4.appendChild(email);
-                td5.appendChild(phone);
-                td6.appendChild(website);
+            td1.appendChild(id);
+            td2.appendChild(name);
+            td3.appendChild(username);
+            td4.appendChild(email);
+            td5.appendChild(phone);
+            td6.appendChild(website);
 
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                tr.appendChild(td3);
-                tr.appendChild(td4);
-                tr.appendChild(td5);
-                tr.appendChild(td6);
-
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
             
             document.getElementById("userData").appendChild(tr);
         })
